e2b9971fefa551599a29d92b497ea8578f66b9a3
brandonlee781 <brandonlee781@gmail.com>
2016-11-05 14:48:01 -0500

    Finished url shortener

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var request = require('request');
 var moment = require('moment');
 
 var getMonth = require('./utils/getMonth.js');
+var validUrl = new RegExp(/^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/);
 
 var app = express();
 
@@ -82,8 +83,84 @@ app.get('/whoami/api',function(req,res) {
       'language': language,
       'operating system': os
   }
-  console.log(obj);
   res.send(obj);
+})
+//-------------------- URL Shortener -----------------------//
+app.get('/short',function(req,res) {
+  res.sendFile(path.join(__dirname,'/pages/short.html'));
+})
+
+app.get('/short/new/*',function(req,res) {
+  var url = req.params[0];
+
+  MongoClient.connect(dbLocation, function(err, db) {
+    var collection = db.collection('shortenedUrls');
+
+    if (validUrl.test(url)) {
+      var obj = {
+        originalUrl: url,
+        shortUrl: 'https://bl781-ms.herokuapp.com/short/' + Math.random().toString().substring(2,7)
+      }
+
+      // Check to see if an instance of this url already exists
+      // in the database
+      collection.findOne({originalUrl: obj.originalUrl},function(err,item) {
+        assert.equal(null,err);
+
+        // if the url does already exist
+        if (item) {
+          // create new obj to prevent sending mongo id
+          var newObj = {originalUrl:item.originalUrl,shortUrl:item.shortUrl};
+          // display the url
+          res.send(newObj);
+
+        // if the url does not exist
+        } else {
+          // create a new instance of it
+          collection.insertOne(obj);
+
+          setTimeout(function() {
+            // Fetch the document
+            collection.findOne(obj, function(err, item) {
+              assert.equal(null, err);
+              assert.equal(obj.originalUrl, item.originalUrl);
+              assert.equal(obj.shortUrl, item.shortUrl);
+              db.close();
+            })
+          }, 100)
+        }
+      })
+
+    } else {
+      res.send('Invalid url');
+      db.close();
+    }
+  });
+});
+
+app.get('/short/*',function(req,res) {
+  var id = 'https://bl781-ms.herokuapp.com/short/' + req.params[0];
+  console.log(id);
+  MongoClient.connect(dbLocation, function(err,db) {
+    var collection = db.collection('shortenedUrls');
+
+    // search for a matching short url
+    collection.findOne({shortUrl: id},function(err,docs) {
+
+      // if such a db item exists
+      if (docs) {
+        assert.equal(null,err);
+        assert.equal(id,docs.shortUrl);
+
+        // redirect the user to the original url
+        res.redirect(docs.originalUrl);
+        db.close();
+      } else {
+        res.send('No such short URL exists');
+        db.close();
+      }
+    })
+  })
 })
 
 
